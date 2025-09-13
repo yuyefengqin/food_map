8559db3a9c8820e101db3a780aa99869fede7b92
yuyefengqin <yuyefengqin@outlook.com>
2025-09-13 21:36:54 +0800

    跨域配置类

---
+++ food_map/src/main/java/com/gok/food_map/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.gok.food_map.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+         registry.addMapping("/**")
+                 .allowedOriginPatterns("*")
+                 .allowedHeaders("*")
+                 .allowedMethods("*")
+                 .allowCredentials(true)
+                 .maxAge(3600);
+    }
+}
