9247df8095901754d6b038a5bfca0e2507fffce7
LeiNa <yuyefengqin@outlook.com>
2025-09-30 23:34:05 +0800

    czh修改拦截器错误

--- food_map/src/main/java/com/gok/food_map/config/WebConfig.java
+++ food_map/src/main/java/com/gok/food_map/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.gok.food_map.config;
 
-import com.gok.food_map.log.interceptor.TokenInterceptor;
+
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,13 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
                  .allowCredentials(true)
                  .maxAge(3600);
     }
-    @Resource
-    private TokenInterceptor tokenInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //注册一个token的拦截器
-        registry.addInterceptor(tokenInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/user/login");
-    }
+
 }
