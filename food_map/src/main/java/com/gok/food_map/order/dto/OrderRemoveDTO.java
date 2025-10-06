d757a012ae81000a981da2d60fdcf5d669e60aae
xiao-wang0627 <3274188494@qq.com>
2025-10-06 14:37:49 +0800

    wsy 更新项目

---
+++ food_map/src/main/java/com/gok/food_map/order/dto/OrderRemoveDTO.java
@@ -0,0 +1,8 @@
+package com.gok.food_map.order.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderRemoveDTO {
+    private Long orderId;
+}
---
+++ food_map/src/main/java/com/gok/food_map/user/dto/LevelGetListDTO.java
@@ -0,0 +1,10 @@
+package com.gok.food_map.user.dto;
+
+import lombok.Data;
+
+@Data
+public class LevelGetListDTO {
+    private String current;
+
+    private String size;
+}
